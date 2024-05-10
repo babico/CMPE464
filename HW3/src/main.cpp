277fe80f12fad322573aa32286f584fe9bd8e1ba
Müslüm Barış Korkmazer <hello@babico.name.tr>
2024-05-10 15:39:42 +0300

    Add HW3

---
+++ HW3/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <matplot/matplot.h>
+
+// Necessary for this project
+const std::string IMAGES_PATH = "/home/babico/Projects/CMPE464-Github/HW3/images/";
+
+// int main(int argc, char const *argv[])
+int main()
+{
+	/* code */
+	return 0;
+}
